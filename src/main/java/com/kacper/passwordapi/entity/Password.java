a81242d0be753471fb1e5c6004bbe86dd24da82e
Kacper Sobania <kacper_s11@wp.pl>
2022-12-19 11:16:57 +0100

    Created project structure

---
+++ src/main/java/com/kacper/passwordapi/entity/Password.java
@@ -0,0 +1,7 @@
+package com.kacper.passwordapi.entity;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Password {
+}
---
+++ src/main/java/com/kacper/passwordapi/repository/PasswordRepository.java
@@ -0,0 +1,7 @@
+package com.kacper.passwordapi.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PasswordRepository {
+}
