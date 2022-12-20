9e02c42d04a84b305113af4be7bd84b980fc286b
Kacper Sobania <kacper_s11@wp.pl>
2022-12-20 11:11:18 +0100

    Added data validation

--- src/main/java/com/kacper/passwordapi/controller/PasswordController.java
+++ src/main/java/com/kacper/passwordapi/controller/PasswordController.java
@@ -3,11 +3,16 @@ package com.kacper.passwordapi.controller;
 import com.kacper.passwordapi.dto.GeneratedPasswordDto;
 import com.kacper.passwordapi.dto.PasswordDto;
 import com.kacper.passwordapi.service.PasswordService;
+import jakarta.validation.constraints.Max;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.Size;
 import lombok.RequiredArgsConstructor;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Validated
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/password-api")
@@ -16,17 +21,17 @@ public class PasswordController {
     private final PasswordService passwordService;
 
     @GetMapping("/passwords")
-    public List<GeneratedPasswordDto> createPassword(@RequestParam(name = "lgth") int length, @RequestParam(defaultValue = "false", name = "spclCh") boolean specialCharactersPresence, @RequestParam(defaultValue = "true", name = "lwrCsLet") boolean lowerCaseLetterPresence, @RequestParam(defaultValue = "false", name = "cptCsLet") boolean capitalCaseLetter, @RequestParam(name = "passwords") int numbeOfPasswords){
-        return passwordService.createPassword(length, specialCharactersPresence, lowerCaseLetterPresence, capitalCaseLetter, numbeOfPasswords);
+    public List<GeneratedPasswordDto> createPassword(@RequestParam(name = "lgth") @Min(value = 3, message = "Password must be at least {value} characters long") @Max(value = 32, message = "Password can not be longer than {value} characters") int length, @RequestParam(defaultValue = "false", name = "spclCh") boolean specialCharactersPresence, @RequestParam(defaultValue = "true", name = "lwrCsLet") boolean lowerCaseLetterPresence, @RequestParam(defaultValue = "false", name = "cptCsLet") boolean capitalCaseLetter, @RequestParam(name = "passwords") @Min(value = 1, message = "Number of generated passwords must be at least {value}") @Max(value = 1000, message = "Number of generated passwords can not be higher than {value}") int numberOfPasswords){
+        return passwordService.createPassword(length, specialCharactersPresence, lowerCaseLetterPresence, capitalCaseLetter, numberOfPasswords);
     }
 
     @GetMapping("/verification/{password}")
-    public PasswordDto verifyPassword(@PathVariable String password){
+    public PasswordDto verifyPassword(@PathVariable @Size(min = 3, max = 32, message = "Password length must be between {min} and {max}") String password){
         return passwordService.verifyPassword(password);
     }
 
     @DeleteMapping("/removal/{password}")
-    public PasswordDto removePassword(@PathVariable String password){
+    public PasswordDto removePassword(@PathVariable @Size(min = 3, max = 32, message = "Password length must be between {min} and {max}") String password){
         return passwordService.removePassword(password);
     }
 }
